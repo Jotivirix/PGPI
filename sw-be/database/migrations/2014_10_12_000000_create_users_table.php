8f4d8fbed3d02ff9b2b5f23b92f27c2d55e4ca63
emiliocb <emilio@perto.de>
2021-03-07 13:14:50 +0100

    database users table

--- sw-be/database/migrations/2014_10_12_000000_create_users_table.php
+++ sw-be/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,16 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role');
+            $table->string('phone_number');
+            $table->string('street');
+            $table->string('city');
+            $table->string('zip_code');
+            $table->string('country');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
