5e999d38cdf29d5c6ab73c11389269a36814f1e4
Jotivirix <joignasa@gmail.com>
2021-04-07 17:50:28 +0200

    Cambiado el CSV. Movida la Navbar a un componente de Angular

--- sw-fe/src/app/app.module.ts
+++ sw-fe/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
