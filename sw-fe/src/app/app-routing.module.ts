60ecd146dc940cc6f33ce4fbe0ec4f9f43570ccc
Jotivirix <joignasa@gmail.com>
2021-04-18 17:53:01 +0200

    Pantalla de Producto Especifico Completada

--- sw-fe/src/app/app-routing.module.ts
+++ sw-fe/src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
     component: LoginComponent,
   },
   {
-    path: 'product/:id',
+    path: 'product/:id/:proveedor',
     component: ProductComponent,
   }
 ];
---
+++ sw-fe/src/app/interfaces/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  reference: string,
+  description: string,
+  stock: Number;
+  picking: Number;
+  warning_stock: Number;
+  image: string;
+  provider_id: Number;
+  location_id: string;
+  precio: Number;
+}
