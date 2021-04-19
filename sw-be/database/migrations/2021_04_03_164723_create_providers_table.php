df9f21ed040cd7e4eb643ac94ba2ec4f3ebe4439
Emilio <ecabab@gmail.com>
2021-04-19 16:52:51 +0200

    CSV done

--- sw-be/database/migrations/2021_04_03_164723_create_providers_table.php
+++ sw-be/database/migrations/2021_04_03_164723_create_providers_table.php
@@ -15,13 +15,13 @@ class CreateProvidersTable extends Migration
     {
         Schema::create('providers', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('phone_number');
-            $table->string('street');
-            $table->string('city');
-            $table->string('zip_code');
-            $table->string('country');
+            $table->string('name')->unique();
+            $table->string('email')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('street')->nullable();
+            $table->string('city')->nullable();
+            $table->string('zip_code')->nullable();
+            $table->string('country')->nullable();
             $table->timestamps();
         });
     }
--- sw-be/database/seeders/DatabaseSeeder.php
+++ sw-be/database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            ProductsTableSeeder::class,
+            // ProductsTableSeeder::class,
             ShipmentCompaniesTableSeeder::class
         ]);
     }
