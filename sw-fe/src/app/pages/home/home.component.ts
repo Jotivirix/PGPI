5ab644da981f1ea3d9c5320ddf65a522f86544e2
Jotivirix <joignasa@gmail.com>
2021-04-20 23:45:14 +0200

    Carga de productos desde caché

--- sw-fe/src/app/pages/home/home.component.ts
+++ sw-fe/src/app/pages/home/home.component.ts
@@ -7,7 +7,6 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
   loading: boolean;
 
   productosLocal = [
@@ -1033,14 +1032,52 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.getProductos();
+    //Si hay productos en cache
+    if (this.getWithExpiry('productos_cache') !== null) {
+      this.productos = this.getWithExpiry('productos_cache');
+      console.log('Loaded from cache');
+      this.loading = false;
+    } else {
+      this.getProductos();
+      console.log('Loaded from DB');
+    }
   }
 
   async getProductos(): Promise<any> {
     await this.productService.getProductos().subscribe((res) => {
       this.productos = res.products;
       console.log(this.productos);
+      this.setWithExpiry('productos_cache', this.productos, 120000);
       this.loading = false;
     });
+  }
+
+  setWithExpiry(key: string, value: any, ttl: any) {
+    const now = new Date();
+    // `item` is an object which contains the original value
+    // as well as the time when it's supposed to expire
+    const item = {
+      value: value,
+      expiry: now.getTime() + ttl,
+    };
+    localStorage.setItem(key, JSON.stringify(item));
+  }
+
+  getWithExpiry(key:string):any {
+    const itemStr = localStorage.getItem(key);
+    // if the item doesn't exist, return null
+    if (!itemStr) {
+      return null;
+    }
+    const item = JSON.parse(itemStr);
+    const now = new Date();
+    // compare the expiry time of the item with the current time
+    if (now.getTime() > item.expiry) {
+      // If the item is expired, delete the item from storage
+      // and return null
+      localStorage.removeItem(key);
+      return null;
+    }
+    return item.value;
   }
 }
